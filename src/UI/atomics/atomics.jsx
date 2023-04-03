ce592d6d8c5862b23d191052186a72467aeae3fc
MyungAe <ladiescheong@naver.com>
2023-04-03 21:07:01 +0900

    init(all): 불필요한 파일 삭제, 폴더 구조 변경, 개발용 기본 폰트 적용

---
+++ src/UI/atomics/atomics.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function atomics() {
+  return <div>atomics</div>;
+}
+
+export default atomics;
---
+++ src/UI/molecules/molecules.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function molecules() {
+  return <div>molecules</div>;
+}
+
+export default molecules;
