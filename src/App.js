0c12267affc8ac163a6e52844f1dbae2ccf8272e
MyungAe <ladiescheong@naver.com>
2023-04-11 17:28:38 +0900

    #3 feat(GlobalStyle): styled-components 설치, GlobalStyle 사용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import ReadStudy from './UI/pages/ReadStudy';
+import { GlobalStyle } from './shared/GlobalStyle';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <ReadStudy />
+    </div>
+  );
 }
 
 export default App;
