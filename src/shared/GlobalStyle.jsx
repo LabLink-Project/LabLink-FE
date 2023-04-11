10357a2f266a25d1d6f8c53af45b9ceef5ca5d7b
MyungAe <ladiescheong@naver.com>
2023-04-11 17:28:38 +0900

    #3 feat(GlobalStyle): styled-components 설치, GlobalStyle 사용

--- src/shared/GlobalStyle.jsx
+++ src/shared/GlobalStyle.jsx
@@ -1,1 +1,17 @@
-export const GlobalStyle = '';
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    body {
+        font-family: Pretendard, sans-serif
+    }
+
+    h1, h2, p, ul {
+        margin: 0;
+
+    }
+
+    ul {
+        padding: 0;
+        list-style-type: none
+    }
+`;
