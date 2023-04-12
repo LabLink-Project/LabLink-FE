f414df6a0771777235a04c23e9112a489764bae2
MyungAe <ladiescheong@naver.com>
2023-04-12 19:03:14 +0900

    #3 feat(ReadStudy): 스터디 게시물 태그 컴포넌트로 분리해 구현

--- src/UI/pages/ReadStudy.jsx
+++ src/UI/pages/ReadStudy.jsx
@@ -1,36 +1,48 @@
 import React from 'react';
+import Study from 'src/UI/components/Study';
 
 function ReadStudy() {
-  const mockupData = {
-    id: 1,
-    title: '웹 서비스 테스터 모집',
-    category: 'UT',
-    companyName: 'LabLink',
-    companyHeat: 75,
-    date: '2023-04-03 22:34',
-    pay: '급여',
-    region: '지역',
-    bookmark: true,
-  };
+  const mockupData = [
+    {
+      id: 1,
+      title: 'APP 사용성테스트 지원자 모집',
+      category: 'UT',
+      companyName: '(주)항해99',
+      companyHeat: 75,
+      date: '2023-04-03 22:34',
+      pay: '10000',
+      address: 'online',
+      isBookmarked: true,
+    },
+    {
+      id: 2,
+      title: 'MRI 받아보실 분',
+      category: '실험',
+      companyName: 'KAIST',
+      companyHeat: 90,
+      date: '2023-10-03 22:34',
+      pay: '10000',
+      address: '지역',
+      isBookmarked: false,
+    },
+  ];
 
   return (
     <div>
-      <h1>게시글 전체 조회</h1>
+      <div>뒤로가기 / 알림</div>
+      <div>검색 창</div>
+      <h1>*으로 검색결과 n건</h1>
+      <div>정렬기준(최신순)</div>
       <ul>
-        <li>
-          <article>
-            <h2>제목 : {mockupData.title}</h2>
-            <div>북마크 : {mockupData.bookmark ? 'star' : 'emptystar'}</div>
-            <div>카테고리 : {mockupData.category}</div>
-            <div>
-              <h3>작성자 정보</h3>
-              <p>기업명 : {mockupData.companyName}</p>
-              <p>기업온도 : {mockupData.companyHeat}</p>
-            </div>
-            <div>급여 : {mockupData.pay}</div>
-            <div>지역 : {mockupData.region}</div>
-          </article>
-        </li>
+        {/* 와이어프레임 기준 */}
+        {mockupData.map(obj => {
+          return (
+            <Study
+              obj={obj}
+              key={obj.id}
+            />
+          );
+        })}
       </ul>
     </div>
   );
