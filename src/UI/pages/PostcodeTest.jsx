b1a0dda20dc97d33c1751e0e755fa112921077d1
MyungAe <ladiescheong@naver.com>
2023-04-13 20:28:35 +0900

    #28 feat(PostcodeTest): 우편번호 및 주소 입력에 필요한 패키지 설치, 구현 완료

---
+++ src/UI/pages/PostcodeTest.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import DaumPostcodeEmbed, { useDaumPostcodePopup } from 'react-daum-postcode';
+
+function PostcodeTest() {
+  return (
+    <div>
+      <h1>다음 우편번호 서비스 테스트용입니다.</h1>
+      {/* <EmbedPostcode /> */}
+      <PopupPostcode />
+    </div>
+  );
+}
+
+const EmbedPostcode = props => {
+  const handleComplete = data => {
+    let fullAddress = data.address;
+    let extraAddress = '';
+
+    if (data.addressType === 'R') {
+      if (data.bname !== '') {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== '') {
+        extraAddress +=
+          extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== '' ? `(${extraAddress})` : '';
+    }
+
+    console.log(fullAddress);
+  };
+  return (
+    <DaumPostcodeEmbed
+      onComplete={handleComplete}
+      {...props}
+    />
+  );
+};
+
+const PopupPostcode = props => {
+  const open = useDaumPostcodePopup();
+
+  const handleComplete = data => {
+    let fullAddress = data.address;
+    let extraAddress = '';
+
+    if (data.addressType === 'R') {
+      if (data.bname !== '') {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== '') {
+        extraAddress +=
+          extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== '' ? ` (${extraAddress})` : '';
+    }
+
+    console.log(fullAddress); // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
+  };
+
+  const handleClick = () => {
+    open({ onComplete: handleComplete });
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={handleClick}
+      {...props}
+    >
+      Open
+    </button>
+  );
+};
+
+export default PostcodeTest;
