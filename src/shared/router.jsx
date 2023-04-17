2a0147b7cc68784aeac71fdf65cdd8ea26717cc7
MyungAe <ladiescheong@naver.com>
2023-04-18 03:39:51 +0900

    #39 feat(PersonalRequest): 신청 목록 UI 구현 완료

--- src/shared/router.jsx
+++ src/shared/router.jsx
@@ -12,6 +12,7 @@ import ReadStudyDetail from 'src/UI/pages/ReadStudyDetail';
 import BookmarkList from 'src/UI/pages/BookmarkList';
 import PersonalPage from 'src/UI/pages/PersonalPage';
 import EditPersonalProfile from 'src/UI/pages/EditPersonalProfile';
+import PersonalRequest from 'src/UI/pages/PersonalRequest';
 
 const Router = () => {
   return (
@@ -36,6 +37,10 @@ const Router = () => {
         <Route
           path="/mypage/check"
           element={<EditPersonalProfile />}
+        />
+        <Route
+          path="/mypage/request"
+          element={<PersonalRequest />}
         />
         <Route
           path="/signup"
