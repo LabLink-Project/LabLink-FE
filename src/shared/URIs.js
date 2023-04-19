49f19fb68b9a0732946bb92ecea1c99e8288f2e3
MyungAe <ladiescheong@naver.com>
2023-04-20 00:55:28 +0900

    #44 feat(URIs): 라우터와 Link 경로를 관리할 URI 객체 생성

---
+++ src/shared/URIs.js
@@ -0,0 +1,40 @@
+export const URI = {
+  crud: {
+    home: '/',
+    search: '/search',
+    studys: '/studys',
+    studyDetail: '/studys/:id',
+    bookmark: '/bookmarks',
+  },
+  auth: {
+    signin: {
+      home: '/auth/signin',
+      user: '/auth/signin/user',
+      company: '/auth/signin/company',
+    },
+    signup: {
+      home: '/auth/signup',
+      user: '/auth/signup/user',
+      userTerm: '/auth/signup/user/term',
+      company: 'auth/signup/company',
+      companyTerm: 'auth/signup/company/term',
+      done: 'auth/signup/done',
+      findID: 'auth/findid',
+    },
+  },
+  mypage: {
+    user: {
+      home: '/mypage/user',
+      editInfo: '/mypage/user/editinfo',
+      apply: '/mypage/user/apply',
+      application: '/mypage/user/application',
+    },
+    company: {
+      home: '/mypage/company',
+      myStudys: '/mypage/company/studys',
+      applicant: '/mypage/company/applicant',
+      feedback: '/mypage/company/feedbacks',
+      makeStudy: '/mypage/company/makestudy',
+    },
+  },
+};
