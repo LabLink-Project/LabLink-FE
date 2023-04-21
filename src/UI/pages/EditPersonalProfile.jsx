8b7e9baa22e7694170ec4a32e5a67858542ce3e8
MyungAe <ladiescheong@naver.com>
2023-04-22 01:32:41 +0900

    #50 feat(EditPersonalProfile): 내 정보 UI 구성 수정, 불러오기 기능 일부 구현

--- src/UI/pages/EditPersonalProfile.jsx
+++ src/UI/pages/EditPersonalProfile.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchHeader from '../components/SearchHeader';
 import {
   StEditPersonalProfileButton,
@@ -7,6 +7,7 @@ import {
   StEditPersonalProfileCheckPw,
   StEditPersonalProfileDate,
   StEditPersonalProfileDateWrap,
+  StEditPersonalProfileFixedButton,
   StEditPersonalProfileH2,
   StEditPersonalProfileInput,
   StEditPersonalProfilePwWrap,
@@ -20,115 +21,148 @@ import { StTopButton } from '../styles/TopButton.styled';
 import CreateStudyAddress from '../molecules/CreateStudyAddress';
 import { StCreateStudyAddressInput } from '../styles/CreateStudy.styled';
 import { StBlackButton } from '../styles/Button.styled';
+import { apiWithJWT } from 'src/api/api';
 
 function EditPersonalProfile() {
+  const [isCorrectPw, setIsCorrectPw] = useState(false);
+  const [password, setPassword] = useState('');
+
+  const checkIsCorrectPw = async () => {
+    const response = await apiWithJWT
+      .post('/users/check', {
+        password,
+      })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
+
   return (
     <StEditPersonalProfileWrap>
       <SearchHeader title="내 정보 수정" />
-      {/* 비밀번호 체크 */}
-      <StEditPersonalProfilePwWrap>
-        <StEditPersonalProfileH2>비밀번호 확인</StEditPersonalProfileH2>
-        <StEditPersonalProfileInput
-          type="text"
-          placeholder="비밀번호 입력"
-        />
-        <StEditPersonalProfileValidationMessage>
-          비밀번호가 일치하지 않습니다
-        </StEditPersonalProfileValidationMessage>
-      </StEditPersonalProfilePwWrap>
-      {/* 내 정보 수정 */}
-      <StFlexBox>
-        <StTopButton>내 정보 수정</StTopButton>
-        <StTopButton>비밀번호 변경</StTopButton>
-      </StFlexBox>
-      <div>
-        <StEditPersonalProfileTitle>이름</StEditPersonalProfileTitle>
-        <StEditPersonalProfileChangeInfoInput
-          type="text"
-          placeholder="이름을 입력해 주세요."
-        />
-      </div>
-      <div>
-        <StEditPersonalProfileTitle>생년월일</StEditPersonalProfileTitle>
-        <StEditPersonalProfileDateWrap>
-          <StEditPersonalProfileYear type="number" />
-          <StEditPersonalProfileDate type="number" />
-          <StEditPersonalProfileDate type="number" />
-        </StEditPersonalProfileDateWrap>
-      </div>
-      <div>
-        <StEditPersonalProfileTitle>성별</StEditPersonalProfileTitle>
-        <StEditPersonalProfileDateWrap>
-          <StEditPersonalProfileButton
-            type="button"
-            value="남자"
-          />
-          <StEditPersonalProfileButton
-            type="button"
-            value="여자"
-          />
-        </StEditPersonalProfileDateWrap>
-      </div>
-      <div>
-        <StEditPersonalProfileTitle>휴대폰 번호</StEditPersonalProfileTitle>
-        <StCreateStudyAddressInput
-          type="text"
-          //   defaultValue={study.address}
-          placeholder="휴대폰 번호 - 제외하고 입력"
-          disabled
-          // onChange={e => setStudy({ ...study, address: e.target.value })}
-        />
 
-        <StBlackButton
-          onClick={e => {
-            e.preventDefault();
-            // modalHandler();
-          }}
-        >
-          인증번호
-        </StBlackButton>
-        <StEditPersonalProfileCheckDiv>
-          인증번호가 전송되었습니다
-        </StEditPersonalProfileCheckDiv>
-        <StCreateStudyAddressInput
-          type="text"
-          //   defaultValue={study.address}
-          placeholder="인증번호 입력"
-          disabled
-          // onChange={e => setStudy({ ...study, address: e.target.value })}
-        />
+      {!isCorrectPw ? (
+        <>
+          {/* 비밀번호 체크 */}
+          <StEditPersonalProfilePwWrap>
+            <StEditPersonalProfileH2>비밀번호 확인</StEditPersonalProfileH2>
+            <StEditPersonalProfileInput
+              type="password"
+              placeholder="비밀번호 입력"
+              onChange={e => {
+                setPassword(e.target.value);
+              }}
+            />
+            <StEditPersonalProfileValidationMessage>
+              비밀번호가 일치하지 않습니다
+            </StEditPersonalProfileValidationMessage>
+            <StEditPersonalProfileFixedButton onClick={checkIsCorrectPw}>
+              저장하기
+            </StEditPersonalProfileFixedButton>
+          </StEditPersonalProfilePwWrap>
+        </>
+      ) : (
+        <>
+          {/* 내 정보 수정
+          <StFlexBox>
+            <StTopButton>내 정보 수정</StTopButton>
+            <StTopButton>비밀번호 변경</StTopButton>
+          </StFlexBox>
+          <div>
+            <StEditPersonalProfileTitle>이름</StEditPersonalProfileTitle>
+            <StEditPersonalProfileChangeInfoInput
+              type="text"
+              placeholder="이름을 입력해 주세요."
+            />
+          </div>
+          <div>
+            <StEditPersonalProfileTitle>생년월일</StEditPersonalProfileTitle>
+            <StEditPersonalProfileDateWrap>
+              <StEditPersonalProfileYear type="number" />
+              <StEditPersonalProfileDate type="number" />
+              <StEditPersonalProfileDate type="number" />
+            </StEditPersonalProfileDateWrap>
+          </div>
+          <div>
+            <StEditPersonalProfileTitle>성별</StEditPersonalProfileTitle>
+            <StEditPersonalProfileDateWrap>
+              <StEditPersonalProfileButton
+                type="button"
+                value="남자"
+              />
+              <StEditPersonalProfileButton
+                type="button"
+                value="여자"
+              />
+            </StEditPersonalProfileDateWrap>
+          </div>
+          <div>
+            <StEditPersonalProfileTitle>휴대폰 번호</StEditPersonalProfileTitle>
+            <StCreateStudyAddressInput
+              type="text"
+              //   defaultValue={study.address}
+              placeholder="휴대폰 번호 - 제외하고 입력"
+              disabled
+              // onChange={e => setStudy({ ...study, address: e.target.value })}
+            />
 
-        <StBlackButton
-          onClick={e => {
-            e.preventDefault();
-            // modalHandler();
-          }}
-        >
-          확인
-        </StBlackButton>
-        <StEditPersonalProfileCheckDiv>
-          인증번호가 일치하지 않습니다
-        </StEditPersonalProfileCheckDiv>
-      </div>
-      <CreateStudyAddress title="주소" />
-      {/* 비밀번호 변경 */}
-      <div>
-        <StEditPersonalProfileTitle>비밀번호 변경</StEditPersonalProfileTitle>
-        <StEditPersonalProfileCheckPw
-          type="text"
-          placeholder="비밀번호 입력"
-        />
-        <StEditPersonalProfileCheckDiv>
-          비밀번호는 대/소문자, 숫자, 특수문자를 포함해 주세요
-        </StEditPersonalProfileCheckDiv>
-        <StEditPersonalProfileCheckPw
-          type="text"
-          placeholder="패스워드 확인"
-        />
-        <StEditPersonalProfileCheckDiv>
-          비밀번호가 일치하지 않습니다
-        </StEditPersonalProfileCheckDiv>
-      </div>
+            <StBlackButton
+              onClick={e => {
+                e.preventDefault();
+                // modalHandler();
+              }}
+            >
+              인증번호
+            </StBlackButton>
+            <StEditPersonalProfileCheckDiv>
+              인증번호가 전송되었습니다
+            </StEditPersonalProfileCheckDiv>
+            <StCreateStudyAddressInput
+              type="text"
+              //   defaultValue={study.address}
+              placeholder="인증번호 입력"
+              disabled
+              // onChange={e => setStudy({ ...study, address: e.target.value })}
+            />
+
+            <StBlackButton
+              onClick={e => {
+                e.preventDefault();
+                // modalHandler();
+              }}
+            >
+              확인
+            </StBlackButton>
+            <StEditPersonalProfileCheckDiv>
+              인증번호가 일치하지 않습니다
+            </StEditPersonalProfileCheckDiv>
+          </div>
+          <CreateStudyAddress title="주소" /> */}
+
+          {/* 비밀번호 변경 */}
+          {/* <div>
+            <StEditPersonalProfileTitle>
+              비밀번호 변경
+            </StEditPersonalProfileTitle>
+            <StEditPersonalProfileCheckPw
+              type="text"
+              placeholder="비밀번호 입력"
+            />
+            <StEditPersonalProfileCheckDiv>
+              비밀번호는 대/소문자, 숫자, 특수문자를 포함해 주세요
+            </StEditPersonalProfileCheckDiv>
+            <StEditPersonalProfileCheckPw
+              type="text"
+              placeholder="패스워드 확인"
+            />
+            <StEditPersonalProfileCheckDiv>
+              비밀번호가 일치하지 않습니다
+            </StEditPersonalProfileCheckDiv>
+          </div>
+          <StEditPersonalProfileFixedButton>
+            수정완료
+          </StEditPersonalProfileFixedButton> */}
+        </>
+      )}
     </StEditPersonalProfileWrap>
   );
 }
