5da966c78db8af32d01d85bf489f3852d52dd712
MyungAe <ladiescheong@naver.com>
2023-04-22 01:28:09 +0900

    #50 feat(FooterNav): 하단 메뉴를 채팅에서 글 작성으로 변경

--- src/UI/components/FooterNav.jsx
+++ src/UI/components/FooterNav.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { StFooterNavList, StFooterNavUl } from '../styles/FooterNav.styled';
 import { Link } from 'react-router-dom';
 import { URI } from 'src/shared/URIs';
+import { useSelector } from 'react-redux';
 
 function FooterNav() {
+  const accountState = useSelector(state => state);
+  const { nickname, ...rest } = accountState.accountHandler;
+
   return (
     <footer>
       <nav>
@@ -14,9 +18,18 @@ function FooterNav() {
           <StFooterNavList>
             <Link to={URI.crud.bookmark}>찜목록</Link>
           </StFooterNavList>
+          {/* 글 작성 위치 구현되면 채팅으로 변경하기 */}
           {/* 채팅 구현 후 경로 설정하기 */}
           <StFooterNavList>
-            <Link to="/">채팅</Link>
+            {nickname ? (
+              <>
+                <Link to={URI.mypage.company.createStudy}>글 작성</Link>
+              </>
+            ) : (
+              <>
+                <Link to={URI.auth.signin.user}>글 작성</Link>
+              </>
+            )}
           </StFooterNavList>
           <StFooterNavList>
             <Link to={URI.mypage.user.home}>내 정보</Link>
