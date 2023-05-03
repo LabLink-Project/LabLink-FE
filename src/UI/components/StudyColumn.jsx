890a92464457c4a485afe926f40bf881c74ecbf9
MyungAe <ladiescheong@naver.com>
2023-05-03 20:24:35 +0900

    fix(StudyColumn): 인기 공고 클릭 시 페이지 이동 안되는 부분 수정

--- src/UI/components/StudyColumn.jsx
+++ src/UI/components/StudyColumn.jsx
@@ -9,6 +9,7 @@ import CompanyName from '../atomics/popularStudy/CompanyName';
 import Title from '../atomics/popularStudy/Title';
 import Deadline from '../atomics/popularStudy/Deadline';
 import Pay from '../atomics/popularStudy/Pay';
+import { URI } from 'src/shared/URIs';
 
 // 합성 컴포넌트 패턴으로 처리해보자 도전!!
 function StudyColumn({ obj }) {
@@ -23,7 +24,7 @@ function StudyColumn({ obj }) {
           />
         </StFlexWrap>
         <StWrap>
-          <Link>
+          <Link to={`${URI.crud.studys}/${obj.id}`}>
             <CompanyName>{obj.companyName}</CompanyName>
             <Title>{obj.title}</Title>
           </Link>
